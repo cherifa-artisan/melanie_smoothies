e96b2169810fd1ff5691da988b656aed63f86f77
cherifa-artisan <bella.lamia3@gmail.com>
2025-05-30 10:15:14 +0200

    Update streamlit_app.py
    add search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ from snowflake.snowpark.functions import col
 import requests
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 cnx = st.connection('snowflake')
 session = cnx.session()
@@ -22,9 +23,9 @@ st.write('The name on your smoothie will be: ', name_on_order)
 
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients = st.multiselect('Choose up to 5 ingredients: ', my_dataframe, max_selections=5)
 
