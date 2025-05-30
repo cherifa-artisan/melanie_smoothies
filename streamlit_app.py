fada8b806be5753a93920b4f0c84621195f092e7
cherifa-artisan <bella.lamia3@gmail.com>
2025-05-30 09:52:57 +0200

    Update streamlit_app.py
    typo on snowfake

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 
-cnx = st.connection('Snowflake')
+cnx = st.connection('snowflake')
 session = cnx.session()
 
 ingredients_string = ''
